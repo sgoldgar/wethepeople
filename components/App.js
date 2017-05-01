ea304fa8ca5535711d9dfc2a84013b76e80a9226
Jacob Bodkin <bodkin.j@gmail.com>
2017-04-30 20:12:06 -0500

    completed main nav and structured linked pages

--- components/App.js
+++ components/App.js
@@ -13,6 +13,7 @@ import {
 import Home from './Home';
 import ChangeLocal from './ChangeLocal';
 import GoVote from './GoVote';
+import Header from './Header'
 
 
 class App extends Component {
@@ -30,62 +31,80 @@ class App extends Component {
 
   render() {
     return(
-        <TabBarIOS
-          barTintColor="#512DA8"
-          tintColor="white"
-          unselectedItemTintColor="black"
-          unselectedTintColor="black"
-          >
+        <View style={ styles.app }>
+          <TabBarIOS
+            barTintColor="#512DA8"
+            tintColor="white"
+            unselectedItemTintColor="black"
+            unselectedTintColor="black"
+            >
 
-          <Icon.TabBarItem
-            title="Home"
-            iconName="md-home"
-            selectedIconName="md-home"
-            selected={this.state.selectedTab === 'home'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'home'
-              });
-            }}>
-            <Home />
-          </Icon.TabBarItem>
+            <Icon.TabBarItem
+              title="Home"
+              iconName="md-home"
+              selectedIconName="md-home"
+              selected={this.state.selectedTab === 'home'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'home'
+                });
+              }}>
+              <View style={ styles.appContainer }>
+                <Home />
+              </View>
+            </Icon.TabBarItem>
 
-          <Icon.TabBarItem
-            title="Change Zip"
-            iconName="ios-pin"
-            selectedIconName="ios-pin"
-            selected={this.state.selectedTab === 'changeLocal'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'changeLocal'
-              });
-            }}>
-            <ChangeLocal />
-          </Icon.TabBarItem>
+            <Icon.TabBarItem
+              title="Change Zip"
+              iconName="ios-pin"
+              selectedIconName="ios-pin"
+              selected={this.state.selectedTab === 'changeLocal'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'changeLocal'
+                });
+              }}>
 
-          <Icon.TabBarItem
-            title="Go Vote"
-            iconName="md-checkmark-circle-outline"
-            selectedIconName="md-checkmark-circle-outline"
-            selected={this.state.selectedTab === 'goVote'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'goVote'
-              });
-            }}>
-            <GoVote />
-          </Icon.TabBarItem>
+              <View style={ styles.appContainer }>
+                <Header />
+                <ChangeLocal />
+              </View>
 
-        </TabBarIOS>
+            </Icon.TabBarItem>
+
+            <Icon.TabBarItem
+              title="Go Vote"
+              iconName="md-checkmark-circle-outline"
+              selectedIconName="md-checkmark-circle-outline"
+              selected={this.state.selectedTab === 'goVote'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'goVote'
+                });
+              }}>
+              <View style={ styles.appContainer }>
+                <Header />
+                <GoVote />
+              </View>
+            </Icon.TabBarItem>
+
+          </TabBarIOS>
+        </View>
 
     )
   }
 }
 
 const styles = StyleSheet.create({
-  mainTabBar: {
-    backgroundColor: '#512DA8',
+  app: {
+    flex: 1
+  },
+  appContainer: {
+    flex: 1,
+    flexDirection: 'column',
+
   }
+
 });
 
 export default App;
---
+++ components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  TabBarIOS
+} from 'react-native';
+
+
+class Home extends Component {
+
+
+
+
+  render() {
+    return(
+
+        <View style={ styles.header }>
+          <Text style={ styles.headerText }>We the People...</Text>
+        </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#D1C4E9',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  headerText: {
+    fontFamily: 'Zapfino',
+    flex: 1,
+    marginTop: 20,
+  }
+});
+
+export default Home;
