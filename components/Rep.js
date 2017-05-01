5170732d93b905cd44b435ff7419cbc056fcf160
melissa pringle <melissa.a.acree@gmail.com>
2017-05-01 09:32:26 -0500

    started layout on rep page

---
+++ components/Rep.js
@@ -0,0 +1,33 @@
+import  React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+class Rep extends Component {
+  constructor(props){
+    super(props)
+    this.state=this.defaultState()
+  }
+
+  render(){
+    return(
+      <Image source={require('url')} />
+        name = {first_name + last_name}
+        party = {party}
+        chamber={chamber}
+        phone = {phone}
+        email= {url}
+
+      )
+  }
+}
+
+
+
+
+
+
+
+
+
+export default Rep
+
+
