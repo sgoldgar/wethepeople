10bc13f1bd0017ef70c3a37eb22c52a876b00bdd
Sarah Goldgar <sgoldgar@gmail.com>
2017-05-02 10:56:15 -0500

    broke our replist and federalreps, there is an error in federalreps i need to fix still

--- components/FederalReps.js
+++ components/FederalReps.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RepList from './RepList';
 
 import {
   AppRegistry,
@@ -8,10 +9,44 @@ import {
   TabBarIOS
 } from 'react-native';
 
-import RepList from './RepList';
+
+const googleApiKey = 'AIzaSyB6NrpRei3RgGwXreJOfnM3f9hSeX1wtns';
+const googleApiUrl = 'https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyB6NrpRei3RgGwXreJOfnM3f9hSeX1wtns';
+const addressTest = '&address=1200%20Bentwood%20Rd.%20Austin%20TX';
+const roles = '&roles=legislatorUpperBody';
 
 
 class FederalReps extends Component {
+  construtor(props) {
+    super(props)
+    this.state = {
+      reps: [],
+      address: {
+        street: this.props.address.street,
+        city: this.props.address.city,
+        state: this.props.address.state,
+        zipCode: this.props.address.zipCode
+      }
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyB6NrpRei3RgGwXreJOfnM3f9hSeX1wtns&address=1200%20Bentwood%20Rd.%20Austin%20TX&roles=legislatorUpperBody')
+      .then((response) => {
+        return response.json()
+        console.log(response.json());
+      })
+      .then((response) => {
+        console.log('response: ',response);
+        console.log('response.officials: ',response.officials);
+
+        this.setState({
+          reps: response.officials
+         });
+
+         console.log('this.state.reps: ', this.state.reps)
+      })
+  }
 
 
 
