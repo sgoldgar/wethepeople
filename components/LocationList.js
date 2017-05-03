d75b3c12cb3d43e47c578c4a68b3831308ade46a
GA Austin <GaAustin@GAs-MacBook-Air-3.local>
2017-05-03 11:33:54 -0500

    Updated LocationList.js

--- components/LocationList.js
+++ components/LocationList.js
@@ -1,2 +1,29 @@
 //this is the list of the polling places
 //import React, { Component } from 'react';
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  TabBarIOS
+} from 'react-native';
+
+class LocationList extends Component {
+  render() {
+    return(
+      <View>
+        <Text>List</Text>
+
+      </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default LocationList;
